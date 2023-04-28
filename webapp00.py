d0c1cae5bd9802d3a9a0c45a1707a714832c4bde
luluzinhoBalatensa <132074848+luluzinhoBalatensa@users.noreply.github.com>
2023-04-28 11:09:34 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,17 +4,17 @@
 import streamlit as st
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Prof. Massaki Igarashi")
+st.title("Lucher Construtora")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Teste de Cabeçalho")
+st.header("Engenharia e Arquitetura")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Bem vindos!")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("Alguns projetos já feitos pela nossa equipe ")
 
-st.info("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.info("https://projetopronto.com.br/wp-content/uploads/casa_por_do_sol.png")
 
 st.warning("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
